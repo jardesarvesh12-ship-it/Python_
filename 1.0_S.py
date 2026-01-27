071e072d858e7f0a7c3276ab1d85a910c6b25c79
jardesarvesh12-ship-it <jardesarvesh12@gmail.com>
2026-01-27 13:45:07 +0530

    Function Programs

--- 1.0_S.py
+++ 1.0_S.py
@@ -266,6 +266,8 @@ print(list1)
 
 # TUPLE
 
+# Tuple is important because tuple has higher performance higher speed and have good memory management.
+
 # To create a tuple with only one item, you have to add a comma after the item, otherwise Python will not recognize it as a tuple.
 thistuple = ("apple",)
 print(type(thistuple))
