5808004526093269246f7db8aeeccd7b6b61fd8b
jardesarvesh12-ship-it <jardesarvesh12@gmail.com>
2026-01-28 23:01:17 +0530

    update

---
+++ Function_progrms/2.Program to check if a number is even or odd.py
@@ -0,0 +1,10 @@
+
+def even_odd(n):
+    if n % 2 == 0 :
+        print("even number: ")
+    else:
+        print("odd number: ")
+
+num = int(input("enter a number: "))
+
+print(" number is : ",even_odd(n))
